61f1e2bbc0f465a732ca5147db7337ab5c542080
Alex Devero <deveroalex@gmail.com>
2016-05-29 15:32:52 +0200

    Implemented canvas with Matrix code.

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,62 @@
       if (this.settings.indexCanvas.length > 0) {
         (function() {
           console.log('Canvas');
+
+          var canvas = document.getElementById('indexCanvas');
+          var ctx = canvas.getContext('2d');
+
+          // Making the canvas full screen
+          canvas.height = window.innerHeight;
+          canvas.width = window.innerWidth;
+
+          // Chinese and other characters - taken from the unicode charset
+          var characters = '田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑1234567890ABCDEFGHIKLMNOPQRSTVXYZ';
+
+          // Converting the string into an array of single characters
+          var text = characters.split('');
+
+          var fontSize = 16;
+          var columns = canvas.width/fontSize; //number of columns for the rain
+
+          // Array of drops - one per column
+          var drops = [];
+
+          // x below is the x coordinate
+          // 1 = y co-ordinate of the drop(same for every drop initially)
+          for (var x = 0; x < columns; x++) {
+            drops[x] = 1;
+          }
+
+          // Drawing the characters
+          function draw() {
+            // Black BG for the canvas
+            // Translucent BG to show trail
+            ctx.fillStyle = 'rgba(255,255,255,.025)';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = 'rgba(50,50,50,.25)'; //green text
+            ctx.font = fontSize + 'px arial';
+
+            // Looping over drops
+            for (var i = 0; i < drops.length; i++) {
+              // Random character to print
+              var textPrint = text[Math.floor(Math.random()*text.length)];
+
+              // x = i*fontSize, y = value of drops[i]*fontSize
+              ctx.fillText(textPrint, i*fontSize, drops[i]*fontSize);
+
+              // Sending the drop back to the top randomly after it has crossed the screen
+              // Adding a randomness to the reset to make the drops scattered on the Y axis
+              if (drops[i]*fontSize > canvas.height && Math.random() > 0.975) {
+                drops[i] = 0;
+              }
+
+              // Incrementing Y coordinate
+              drops[i]++;
+            }
+          }
+
+          setInterval(draw, 55);
         })();
       }
 
