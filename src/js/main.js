0e365871b457d516686d38ddb42ad7d7d3c2f073
Alex Devero <deveroalex@gmail.com>
2016-07-11 09:16:01 +0200

    Added font loading scripts.

--- src/js/main.js
+++ src/js/main.js
@@ -55,6 +55,27 @@
         }
 
         window.onload = function() {
+          // Load Font Awesome
+          var stylesheetAwesome = document.createElement('link');
+          stylesheetAwesome.rel = 'stylesheet';
+          stylesheetAwesome.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css';
+          stylesheetAwesome.classList.add('jsLoaded');
+          document.getElementsByTagName('head')[0].appendChild(stylesheetAwesome);
+
+          // Load Open Sans font
+          var stylesheetOpenSans = document.createElement('link');
+          stylesheetOpenSans.rel = 'stylesheet';
+          stylesheetOpenSans.href = 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300';
+          stylesheetOpenSans.classList.add('jsLoaded');
+          document.getElementsByTagName('head')[0].appendChild(stylesheetOpenSans);
+
+          // Load Raleway font
+          var stylesheetRaleway = document.createElement('link');
+          stylesheetRaleway.rel = 'stylesheet';
+          stylesheetRaleway.href = 'https://fonts.googleapis.com/css?family=Raleway:400,300,700,200';
+          stylesheetRaleway.classList.add('jsLoaded');
+          document.getElementsByTagName('head')[0].appendChild(stylesheetRaleway);
+
           var el = document.querySelector('html');
           //el.style.display = 'none';
 
