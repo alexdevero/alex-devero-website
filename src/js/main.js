de945b59222fd70a676934941eaaffa761128e45
Alex Devero <deveroalex@gmail.com>
2016-07-28 07:46:42 +0200

    Added waypoints from Šumava template.

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,8 @@
       indexCanvas: document.querySelectorAll('#indexCanvas'),
       lazyImages: document.getElementsByClassName('lazy'),
       contactForm: document.querySelectorAll('#contactForm'),
-      portfolioItem: document.querySelectorAll('.work__item')
+      portfolioItem: document.querySelectorAll('.work__item'),
+      waypoint: document.querySelectorAll('.waypoint')
     },
     controllers: function() {
 
@@ -330,6 +331,55 @@
           }
         })();
       }*/
+
+      /**
+       * Waypoints
+       * info: http://imakewebthings.com/waypoints/
+       */
+      if (this.settings.waypoint.length > 0) {
+        (function() {
+          $('.wp-1').waypoint(function() {
+            $('.wp-1').addClass('animated fadeInUp');
+          }, {
+            offset: '75%'
+          });
+          $('.wp-2').waypoint(function() {
+            $('.wp-2').addClass('animated fadeInUp');
+          }, {
+            offset: '75%'
+          });
+          $('.wp-3').waypoint(function() {
+            $('.wp-3').addClass('animated fadeInUp');
+          }, {
+            offset: '75%'
+          });
+          $('.wp-4').waypoint(function() {
+            $('.wp-4').addClass('animated fadeIn');
+          }, {
+            offset: '75%'
+          });
+          $('.wp-5').waypoint(function() {
+            $('.wp-5').addClass('animated fadeInRight');
+          }, {
+            offset: '50%'
+          });
+          $('.wp-6').waypoint(function() {
+            $('.wp-6').addClass('animated fadeInLeft');
+          }, {
+            offset: '50%'
+          });
+          $('.wp-7').waypoint(function() {
+            $('.wp-7').addClass('animated fadeInUp');
+          }, {
+            offset: '60%'
+          });
+          $('.wp-8').waypoint(function() {
+            $('.wp-8').addClass('animated fadeInUp');
+          }, {
+            offset: '60%'
+          });
+        })()
+      }
     },
     init: function() {
       //document.querySelector('html').style.display = 'none';
