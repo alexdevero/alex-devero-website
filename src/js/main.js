71103ab9edd6cd8fa649449311da99110f18b83f
Alex Devero <deveroalex@gmail.com>
2016-08-03 09:21:51 +0200

    LazyImages function moved to controllers.

--- src/js/main.js
+++ src/js/main.js
@@ -96,6 +96,46 @@
 
         console.log('fadeOut')
       },
+      // LazyImages controller
+      lazyImages: function() {
+        // Test if image is in the viewport
+        function isImageInViewport(img) {
+          console.log('images')
+          var rect = img.getBoundingClientRect();
+          return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+          );
+        }
+
+        // Add event listeners to images
+        window.addEventListener('DOMContentLoaded', lazyLoadImages);
+        window.addEventListener('load', lazyLoadImages);
+        window.addEventListener('resize', lazyLoadImages);
+        window.addEventListener('scroll', lazyLoadImages);
+
+        // lazyLoadImages function
+        function lazyLoadImages() {
+          var lazyImagesArray = document.querySelectorAll('img[data-src]');
+          for (var i = 0; i < lazyImagesArray.length; i++) {
+            if (isImageInViewport(lazyImagesArray[i])) {
+              lazyImagesArray[i].setAttribute('src', lazyImagesArray[i].getAttribute('data-src'));
+              lazyImagesArray[i].removeAttribute('data-src');
+              lazyImagesArray[i].setAttribute('data-loaded', true);
+            }
+          }
+
+          // Remove event listeners if all images are loaded
+          if (lazyImagesArray.length == 0) {
+            window.removeEventListener('DOMContentLoaded', lazyLoadImages);
+            window.removeEventListener('load', lazyLoadImages);
+            window.removeEventListener('resize', lazyLoadImages);
+            window.removeEventListener('scroll', lazyLoadImages);
+          }
+        }
+      },
       // Waypoints controller
       waypoints: function() {
         // setTimeout function is used to let the dom be loaded.
@@ -405,7 +445,6 @@
        */
       if (this.settings.contactForm.length > 0) {
         (function() {
-          console.log('running');
           window.controllers.contact();
           /*$('#contactForm').submit(function(e) {
             e.preventDefault();
@@ -456,8 +495,9 @@
        */
       if (this.settings.lazyImages.length > 0) {
         (function() {
+          window.controllers.lazyImages();
           // Test if image is in the viewport
-          function isImageInViewport(img) {
+          /*function isImageInViewport(img) {
             var rect = img.getBoundingClientRect();
             return (
               rect.top >= 0 &&
@@ -490,7 +530,7 @@
               window.removeEventListener('resize', lazyLoadImages);
               window.removeEventListener('scroll', lazyLoadImages);
             }
-          }
+          }*/
         })();
       }
 
