0109f2802d03818a8b1b1bb6661b8861978d3e53
Alex Devero <deveroalex@gmail.com>
2016-08-04 12:55:52 +0200

    Removed 'disabled' script for lazy images.

--- src/js/main.js
+++ src/js/main.js
@@ -365,41 +365,6 @@
       if (this.settings.lazyImages.length > 0) {
         (function() {
           window.controllers.lazyImages();
-          // Test if image is in the viewport
-          /*function isImageInViewport(img) {
-            var rect = img.getBoundingClientRect();
-            return (
-              rect.top >= 0 &&
-              rect.left >= 0 &&
-              rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-              rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
-          }
-
-          // Add event listeners to images
-          window.addEventListener('DOMContentLoaded', lazyLoadImages);
-          window.addEventListener('load', lazyLoadImages);
-          window.addEventListener('resize', lazyLoadImages);
-          window.addEventListener('scroll', lazyLoadImages);
-
-          // lazyLoadImages function
-          function lazyLoadImages() {
-            var lazyImagesArray = document.querySelectorAll('img[data-src]');
-            for (var i = 0; i < lazyImagesArray.length; i++) {
-              if (isImageInViewport(lazyImagesArray[i])) {
-                lazyImagesArray[i].setAttribute('src', lazyImagesArray[i].getAttribute('data-src'));
-                lazyImagesArray[i].removeAttribute('data-src');
-              }
-            }
-
-            // Remove event listeners if all images are loaded
-            if (lazyImagesArray.length == 0) {
-              window.removeEventListener('DOMContentLoaded', lazyLoadImages);
-              window.removeEventListener('load', lazyLoadImages);
-              window.removeEventListener('resize', lazyLoadImages);
-              window.removeEventListener('scroll', lazyLoadImages);
-            }
-          }*/
         })();
       }
 
