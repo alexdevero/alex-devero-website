683325fb6c90d0e2ac9c4a6e617838e0727f0052
Alex Devero <deveroalex@gmail.com>
2016-09-05 07:49:02 +0200

    Variables for lazy images switched to 'let'.

--- src/js/main.js
+++ src/js/main.js
@@ -203,7 +203,8 @@
       lazyImages: function() {
         // Test if image is in the viewport
         function isImageInViewport(img) {
-          var rect = img.getBoundingClientRect();
+          let rect = img.getBoundingClientRect();
+
           return (
             rect.top >= 0 &&
             rect.left >= 0 &&
@@ -214,18 +215,25 @@
 
         // Add event listeners to images
         window.addEventListener('DOMContentLoaded', lazyLoadImages);
+
         window.addEventListener('load', lazyLoadImages);
+
         window.addEventListener('resize', lazyLoadImages);
+
         window.addEventListener('scroll', lazyLoadImages);
 
         // lazyLoadImages function
         function lazyLoadImages() {
-          var lazyImagesArray = document.querySelectorAll('img[data-src]');
-          for (var i = 0; i < lazyImagesArray.length; i++) {
+          let lazyImagesArray = document.querySelectorAll('img[data-src]');
+
+          for (let i = 0; i < lazyImagesArray.length; i++) {
             if (isImageInViewport(lazyImagesArray[i])) {
               lazyImagesArray[i].setAttribute('src', lazyImagesArray[i].getAttribute('data-src'));
+
               lazyImagesArray[i].removeAttribute('data-src');
+
               lazyImagesArray[i].setAttribute('data-loaded', true);
+
               window.mainController.fadeInCustom(lazyImagesArray[i]);
             }
           }
@@ -233,8 +241,11 @@
           // Remove event listeners if all images are loaded
           if (lazyImagesArray.length == 0) {
             window.removeEventListener('DOMContentLoaded', lazyLoadImages);
+
             window.removeEventListener('load', lazyLoadImages);
+
             window.removeEventListener('resize', lazyLoadImages);
+
             window.removeEventListener('scroll', lazyLoadImages);
           }
         }
