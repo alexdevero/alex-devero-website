aa63b4fe925b90f43335bac814d52cef9115fb59
Alex Devero <deveroalex@gmail.com>
2017-04-29 09:29:26 +0200

    Implement webpack

--- gulp/javascript.js
+++ gulp/javascript.js
@@ -2,6 +2,8 @@
 
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
+import webpack from 'webpack';
+import webpackStream from 'webpack-stream';
 
 // Concatenate JavaScript files
 gulp.task('js:concat', function() {
@@ -17,13 +19,28 @@ gulp.task('js', () => {
   const babel = require('gulp-babel');
   const browserSync = require('browser-sync');
   const changed = require('gulp-changed');
+  const moduleConfig = {
+    entry: './src/js/main.js',
+    output: {
+      filename: 'main.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+      ],
+    }
+  }
   const rename = require('gulp-rename');
   const uglify = require('gulp-uglify');
 
-  return gulp.src('src/js/main.js')
+  return gulp.src(['src/js/main.js'])
     .pipe(plumber())
     .pipe(changed('dist/js'))
-    .pipe(babel())
+    //.pipe(babel())
+    .pipe(webpackStream(moduleConfig, webpack))
     .pipe(uglify().on('error', (e) => {
       console.log(e + '\r\n There\'s something wrong with the JavaScript file(s).')
     }))
