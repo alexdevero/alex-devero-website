bc760e0da4abe738b8e0ef8fa1c134460a581a75
Alex Devero <deveroalex@gmail.com>
2017-05-21 09:13:06 +0200

    Move custom slider to component

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 
 // Example import
 import { foo } from './components/example.js';
+import { customSlider } from './components/custom-slider.js';
 
 /**
  * @author Alex Devero <deveroalex@gmail.com>
@@ -58,72 +59,7 @@ import { foo } from './components/example.js';
         });
       },
       customSliderController: () => {
-        (() => {
-          let content = document.querySelectorAll('.js-slideable')[0];
-          let anchor = document.querySelectorAll('.js-slideable-anchor')[0];
-
-          content.style.display = 'block';
-
-          content.style.maxHeight = '0';
-
-          content.style.opacity = '0';
-
-          content.style.overflow = 'hidden';
-
-          content.style.transition = 'all 0.3s cubic-bezier(0.4, 0, 1, 1)';
-
-          content.classList.add('slideClosed');
-
-          const customSlideDown = (element) => {
-            element.style.maxHeight = '10000px';
-
-            element.style.opacity = '1';
-
-            if (element.classList.contains('slideClosed')) {
-              element.classList.remove('slideClosed');
-            }
-
-            element.classList.add('slideOpened')
-          }
-
-          const customSlideUp = (element) => {
-            element.style.maxHeight = '0';
-
-            const slideTimer = (seconds, callback) => {
-              let counter = 0;
-
-              let time = window.setInterval(function() {
-                counter++;
-
-                if (counter >= seconds) {
-                  callback();
-
-                  window.clearInterval(time);
-                }
-              }, 10);
-            };
-
-            slideTimer(1, () => {
-              element.style.opacity = '0';
-            });
-
-            if (element.classList.contains('slideOpened')) {
-              element.classList.remove('slideOpened');
-            }
-
-            element.classList.add('slideClosed')
-          }
-
-          anchor.addEventListener('click', (e) => {
-            e.preventDefault();
-
-            if (content.classList.contains('slideClosed')) {
-              customSlideDown(content);
-            } else {
-              customSlideUp(content);
-            }
-          });
-        })()
+        customSlider('.js-slideable', '.js-slideable-anchor');
       },
       // FadeIn controller
       fadeInCustomController: (element) => {
