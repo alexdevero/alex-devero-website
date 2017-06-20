1f40197952e08b18033a9b9801af43d8ab2ac943
Alex Devero <deveroalex@gmail.com>
2017-06-20 12:39:58 +0200

    Remove particlesjs, typed.js and waypoints

--- gulp/copy.js
+++ gulp/copy.js
@@ -26,9 +26,9 @@ gulp.task('copy:jsplugins', () => {
       'src/js/plugins/*',
       '!src/js/plugins/*.rar',
       'node_modules/html5shiv/dist/html5shiv.min.js',
-      'node_modules/particlesjs/dist/particles.min.js',
-      'node_modules/typed.js/dist/typed.min.js',
-      'node_modules/waypoints/lib/noframework.waypoints.min.js'
+      // 'node_modules/particlesjs/dist/particles.min.js',
+      // 'node_modules/typed.js/dist/typed.min.js',
+      // 'node_modules/waypoints/lib/noframework.waypoints.min.js'
     ])
       .pipe(plumber())
       .pipe(changed('dist/js/plugins/'))
