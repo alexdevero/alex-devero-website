b4c21ae809ac48a035b8417abd20ee1c6ed36ae5
Alex Devero <deveroalex@gmail.com>
2017-08-26 12:45:45 +0200

    Add translation for fish info

--- src/js/components/fish-sea.js
+++ src/js/components/fish-sea.js
@@ -78,7 +78,7 @@ const fishSea = () => {
   // one time-step of the timeline loop
   var step = function() {
     // print info
-    info.innerHTML = 'Info:<br>Implementation of autonomous agents<br>combined with genetic algorithms.<br><br>Current population: ' + sea.population.length;
+    info.innerHTML = window.location.href.indexOf('-cs') !== -1 ? 'Info:<br>Implementace autonomních agentů<br>kombinovaných s genetickými algoritmy.<br><br>Současná populace: ' + sea.population.length : 'Info:<br>Implementation of autonomous agents<br>combined with genetic algorithms.<br><br>Current population: ' + sea.population.length;
 
     // clear the screen (with a fade)
     ctx.globalAlpha = 0.8;
